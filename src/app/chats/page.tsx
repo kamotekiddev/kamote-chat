301de06447aa36efb279ad8eed6cd447a4362067
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-05-17 21:37:47 +0800

    build the empty chat state

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import EmptyState from './EmptyState';
 
 const Chats = () => {
-	return <div>page</div>;
+	return (
+		<div className='p-4'>
+			<EmptyState />
+		</div>
+	);
 };
 
 export default Chats;
