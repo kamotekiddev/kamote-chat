6479fcaabf6841d708e6fb96a2c7710e08bddbfa
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-05-18 21:14:37 +0800

    extract empty state into sub component

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -1,15 +1,19 @@
 import { BsChatText } from 'react-icons/bs';
 
-const EmptyState = () => {
+interface Props {
+	title?: string;
+	message?: string;
+}
+const EmptyState = ({ title, message }: Props) => {
 	return (
-		<main className='grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8'>
+		<main className='grid h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8'>
 			<div className='text-center'>
 				<BsChatText className='h-32 w-32 text-gray-900' />
 				<h1 className='mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
-					No Open Coversation
+					{title || 'No Open Coversation'}
 				</h1>
 				<p className='mt-6 text-base leading-7 text-gray-600'>
-					Please select a friend to start with a coversation.
+					{message || 'Please select a friend to start with a coversation.'}
 				</p>
 			</div>
 		</main>
