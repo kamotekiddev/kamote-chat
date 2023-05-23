5a0739486eb5ce5a0dc3ea6b1c4154f810ff2d5c
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-05-23 22:13:16 +0800

    re-implement image implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Input from '@/components/Input';
+import Image from 'next/image';
+
 export default function SignIn() {
 	return (
 		<div className='flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8'>
 			<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-				<img
-					className='mx-auto h-10 w-auto'
+				<Image
+					width={80}
+					height={10}
+					className='mx-auto'
 					src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
 					alt='Your Company'
 				/>
@@ -13,24 +18,7 @@ export default function SignIn() {
 			</div>
 			<div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
 				<form className='space-y-6' action='#' method='POST'>
-					<div>
-						<label
-							htmlFor='email'
-							className='block text-sm font-medium leading-6 text-gray-900'
-						>
-							Email address
-						</label>
-						<div className='mt-2'>
-							<input
-								id='email'
-								name='email'
-								type='email'
-								autoComplete='email'
-								required
-								className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-							/>
-						</div>
-					</div>
+					<Input type='text' label='Email Address' name='email' />
 					<div>
 						<div className='flex items-center justify-between'>
 							<label
@@ -48,16 +36,7 @@ export default function SignIn() {
 								</a>
 							</div>
 						</div>
-						<div className='mt-2'>
-							<input
-								id='password'
-								name='password'
-								type='password'
-								autoComplete='current-password'
-								required
-								className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-							/>
-						</div>
+						<Input type='password' name='password' />
 					</div>
 					<div>
 						<button
