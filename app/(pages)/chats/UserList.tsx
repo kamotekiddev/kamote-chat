f44961623983515a376debaaf4e75b562c559a1b
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-06-05 18:43:17 +0800

    remove the people icon

--- app/(pages)/chats/UserList.tsx
+++ app/(pages)/chats/UserList.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { FiUsers } from 'react-icons/fi';
 import { useRouter } from 'next/navigation';
 
 import UserListItem from './UserListItem';
@@ -20,9 +19,6 @@ const UserList = ({ users }: Props) => {
 		<div className='w-full bg-indigo-50/10 p-4'>
 			<header className='flex items-center justify-between px-4'>
 				<h1 className='prose-lg font-bold'>People</h1>
-				<button className='inline-grid h-8 w-8 place-items-center rounded-lg bg-indigo-50 text-indigo-600 transition-all duration-100 ease-linear hover:bg-gray-200'>
-					<FiUsers />
-				</button>
 			</header>
 			<div className='mt-5 space-y-2'>
 				{users.map((user) => (
