20131c6124c9a3ec56be02e3672d6bd927f02bcb
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-06-05 18:44:59 +0800

    add static active inication on the users list

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -9,6 +9,7 @@ interface Props {
 const Avatar = ({ user }: Props) => {
 	return (
 		<div className='relative h-7 w-7'>
+			<div className='absolute -left-1 -top-1 h-2 w-2 rounded-full bg-green-400' />
 			<Image src={user?.image || userNoPofile} fill alt='Avatar Image' />
 		</div>
 	);
