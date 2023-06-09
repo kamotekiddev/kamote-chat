b20f62f77deeab0dcd94f5fb95f7c56fcf1a3eef
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-06-09 20:26:33 +0800

    re implement typing box

--- app/(pages)/chats/[chatId]/TypingBox.tsx
+++ app/(pages)/chats/[chatId]/TypingBox.tsx
@@ -1,25 +1,27 @@
-import { ComponentProps, FormEvent } from 'react';
+'use client';
 
-interface Props extends ComponentProps<'input'> {
-	onSend: () => void;
-}
+import { FormEvent, useState } from 'react';
 
-const TypingBox = ({ onSend, ...props }: Props) => {
+const TypingBox = () => {
+	const [text, setText] = useState('');
+
 	const handleSend = (e: FormEvent) => {
 		e.preventDefault();
-		onSend();
+		setText('');
 	};
 
 	return (
-		<form onSubmit={handleSend} className='relative flex'>
+		<form onSubmit={handleSend} className='relative flex p-4'>
 			<input
+				value={text}
+				onChange={(e) => setText(e.target.value)}
 				type='text'
+				placeholder='Start Typing'
 				className='w-full rounded-lg border-none bg-indigo-50 p-3 px-4 pr-24 outline-none'
-				{...props}
 			/>
 			<button
 				type='submit'
-				className='absolute right-2 top-1/2 -translate-y-1/2 rounded-lg bg-indigo-600 p-1 px-4 text-indigo-50 transition-colors duration-75 ease-linear hover:bg-indigo-700 hover:text-white'
+				className='absolute right-6 top-1/2 -translate-y-1/2 rounded-lg bg-indigo-600 p-1 px-4 text-indigo-50 transition-colors duration-75 ease-linear hover:bg-indigo-700 hover:text-white'
 			>
 				Send
 			</button>
