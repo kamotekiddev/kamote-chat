21e79043ea0af576f5392133dd235379f0119196
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-06-09 19:16:53 +0800

    add ring to the avatar for visibility

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -8,9 +8,11 @@ interface Props {
 }
 const Avatar = ({ user }: Props) => {
 	return (
-		<div className='relative h-7 w-7'>
-			<div className='absolute -left-1 -top-1 h-2 w-2 rounded-full bg-green-400' />
-			<Image src={user?.image || userNoPofile} fill alt='Avatar Image' />
+		<div className='relative select-none'>
+			<div className='absolute -left-1 -top-2 h-2 w-2 rounded-full bg-green-400' />
+			<div className='relative h-8 w-8 overflow-hidden rounded-full ring-2 ring-indigo-600'>
+				<Image src={user?.image || userNoPofile} fill alt='Avatar Image' />
+			</div>
 		</div>
 	);
 };
