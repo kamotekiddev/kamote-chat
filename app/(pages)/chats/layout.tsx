5c9cb0afe8d6156609f1e6740f1a4e9cb7120c5e
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-06-13 20:49:07 +0800

    able to fetch the list of users to show in the create group chat

--- app/(pages)/chats/layout.tsx
+++ app/(pages)/chats/layout.tsx
@@ -1,4 +1,6 @@
 import getConversations from '@/libs/getConversations';
+import getUsers from '@/libs/getUsers';
+
 import Sidebar from '@/components/Sidebar';
 import ConversationList from './ConversationList';
 
@@ -7,11 +9,12 @@ interface Props {
 }
 const ChatLayout = async ({ children }: Props) => {
 	const conversations = await getConversations();
+	const users = await getUsers();
 
 	return (
 		<main className='grid h-screen grid-cols-[200px_300px_auto]'>
 			<Sidebar />
-			<ConversationList conversations={conversations} />
+			<ConversationList conversations={conversations} users={users} />
 			{children}
 		</main>
 	);
