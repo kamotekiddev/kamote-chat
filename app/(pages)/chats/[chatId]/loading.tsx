3a6eceacf22d7f33fb360ed45f297bb6191c8590
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-06-15 20:21:13 +0800

    feature: add loading states

---
+++ app/(pages)/chats/[chatId]/loading.tsx
@@ -0,0 +1,8 @@
+import LoadingModal from "@/components/LoadingModal";
+import React from "react";
+
+const Loading = () => {
+   return <LoadingModal />;
+};
+
+export default Loading;
--- app/(pages)/people/UserList.tsx
+++ app/(pages)/people/UserList.tsx
@@ -1,40 +1,52 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
-import UserListItem from './UserListItem';
-import { Conversation, User } from '@prisma/client';
+import UserListItem from "./UserListItem";
+import { Conversation, User } from "@prisma/client";
+import LoadingModal from "@/components/LoadingModal";
+import { useState } from "react";
 
 interface Props {
-	users?: User[];
+   users?: User[];
 }
 const UserList = ({ users }: Props) => {
-	const router = useRouter();
+   const router = useRouter();
+   const [isLoading, setLoading] = useState(false);
 
-	const handleSelect = async (user: User) => {
-		const response = await fetch('/api/conversations', {
-			method: 'POST',
-			body: JSON.stringify({ userId: user?.id }),
-		});
-		const data: Conversation = await response.json();
-		if (!data) return null;
-		router.push(`/chats/${data?.id}`);
-	};
+   const handleSelect = async (user: User) => {
+      setLoading(true);
+      const response = await fetch("/api/conversations", {
+         method: "POST",
+         body: JSON.stringify({ userId: user?.id }),
+      });
+      const data: Conversation = await response.json();
+      setLoading(false);
+      if (!data) return null;
+      router.push(`/chats/${data?.id}`);
+   };
 
-	if (!users?.length) return null;
+   if (!users?.length) return null;
 
-	return (
-		<div className='w-full bg-indigo-50/10 p-4'>
-			<header className='px-2'>
-				<h1 className='prose-lg font-bold'>People</h1>
-			</header>
-			<div className='mt-5 space-y-2'>
-				{users.map((user) => (
-					<UserListItem key={user.id} user={user} onSelect={handleSelect} />
-				))}
-			</div>
-		</div>
-	);
+   return (
+      <>
+         {isLoading && <LoadingModal />}
+         <div className="w-full bg-indigo-50/10 p-4">
+            <header className="px-2">
+               <h1 className="prose-lg font-bold">People</h1>
+            </header>
+            <div className="mt-5 space-y-2">
+               {users.map((user) => (
+                  <UserListItem
+                     key={user.id}
+                     user={user}
+                     onSelect={handleSelect}
+                  />
+               ))}
+            </div>
+         </div>
+      </>
+   );
 };
 
 export default UserList;
