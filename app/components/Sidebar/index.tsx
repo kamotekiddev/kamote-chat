5daecd3f1080378b20a195962cdb9129c7b60c21
kamote-kid <kamote-kid@gmail.com>
2023-07-12 11:02:35 +0800

    implement the newly created component user button

--- app/components/Sidebar/index.tsx
+++ app/components/Sidebar/index.tsx
@@ -1,9 +1,13 @@
+import getCurrentUser from "@/libs/getCurrentUser";
 import SidebarList from "./SidebarList";
+import UserButton from "../UserButton";
 
-const Sidebar = () => {
+const Sidebar = async () => {
+  const user = await getCurrentUser();
   return (
-    <nav className="grid grid-rows-[1fr_auto] content-start gap-4 bg-indigo-50/40 p-4">
+    <nav className="grid w-full grid-rows-[1fr_auto] content-start gap-4 bg-indigo-50/40 p-4">
       <SidebarList />
+      <UserButton user={user} />
     </nav>
   );
 };
