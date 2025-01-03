a8c5a23ff33bc41c6403a2cb67294a11d635d673
kamotekid <kamotekiddev@gmail.com>
2025-01-03 21:33:37 +0800

    chore: remove the logo

--- app/(pages)/sign-up/page.tsx
+++ app/(pages)/sign-up/page.tsx
@@ -1,132 +1,127 @@
 "use client";
+
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 import axios, { AxiosError } from "axios";
+import Image from "next/image";
+
 import Input from "@/components/Input";
 
 export default function SignUp() {
-  const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [submitting, setSubmitting] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [error, setError] = useState<AxiosError | undefined>();
+    const router = useRouter();
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [submitting, setSubmitting] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [error, setError] = useState<AxiosError | undefined>();
 
-  const clearForm = () => {
-    setEmail("");
-    setName("");
-    setPassword("");
-  };
+    const clearForm = () => {
+        setEmail("");
+        setName("");
+        setPassword("");
+    };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setSubmitting(true);
-    try {
-      await axios.post("/api/register", {
-        email,
-        name,
-        password,
-      });
-      setIsError(true);
-      setSubmitting(false);
-      clearForm();
-      router.replace("/");
-    } catch (error) {
-      setSubmitting(false);
-      setIsError(true);
-      if (error instanceof AxiosError) setError(error);
-    }
-  };
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setSubmitting(true);
+        try {
+            await axios.post("/api/register", {
+                email,
+                name,
+                password,
+            });
+            setIsError(true);
+            setSubmitting(false);
+            clearForm();
+            router.replace("/");
+        } catch (error) {
+            setSubmitting(false);
+            setIsError(true);
+            if (error instanceof AxiosError) setError(error);
+        }
+    };
 
-  return (
-    <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <Image
-          width={80}
-          height={10}
-          className="mx-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign Up for Account
-        </h2>
-      </div>
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form
-          onSubmit={handleSubmit}
-          className="space-y-6"
-          action="#"
-          method="POST"
-        >
-          <Input
-            label="Email Address"
-            type="email"
-            name="email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            label="Name"
-            type="text"
-            name="name"
-            required
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+    return (
+        <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Sign Up for Account
+                </h2>
+            </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form
+                    onSubmit={handleSubmit}
+                    className="space-y-6"
+                    action="#"
+                    method="POST"
                 >
-                  Forgot password?
-                </a>
-              </div>
+                    <Input
+                        label="Email Address"
+                        type="email"
+                        name="email"
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Input
+                        label="Name"
+                        type="text"
+                        name="name"
+                        required
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label
+                                htmlFor="password"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                            >
+                                Password
+                            </label>
+                            <div className="text-sm">
+                                <a
+                                    href="#"
+                                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                                >
+                                    Forgot password?
+                                </a>
+                            </div>
+                        </div>
+                        <Input
+                            type="text"
+                            name="name"
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    {isError && (
+                        <div className="rounded-lg bg-rose-500 p-2 px-4 text-white">
+                            {error?.response?.data as string}
+                        </div>
+                    )}
+                    <div>
+                        <button
+                            type="submit"
+                            disabled={submitting}
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:cursor-default disabled:opacity-50"
+                        >
+                            {submitting ? "Signing Up..." : "Sign Up"}
+                        </button>
+                    </div>
+                </form>
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    Already a member?
+                    <a
+                        href="/"
+                        className="ml-2 font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                    >
+                        Sign In
+                    </a>
+                </p>
             </div>
-            <Input
-              type="text"
-              name="name"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          {isError && (
-            <div className="rounded-lg bg-rose-500 p-2 px-4 text-white">
-              {error?.response?.data as string}
-            </div>
-          )}
-          <div>
-            <button
-              type="submit"
-              disabled={submitting}
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:cursor-default disabled:opacity-50"
-            >
-              {submitting ? "Signing Up..." : "Sign Up"}
-            </button>
-          </div>
-        </form>
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Already a member?
-          <a
-            href="/"
-            className="ml-2 font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            Sign In
-          </a>
-        </p>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,164 +9,168 @@ import Image from "next/image";
 import Input from "@/components/Input";
 
 interface Credentials {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 export default function SignIn() {
-  const { status } = useSession();
-  const [creds, setCreds] = useState<Credentials>({
-    email: "",
-    password: "",
-  });
-  const router = useRouter();
-  const [submitting, setSubmiting] = useState(false);
-  const [error, setError] = useState("");
+    const router = useRouter();
+    const { status } = useSession();
 
-  useEffect(() => {
-    if (status === "authenticated") router.push("/chats");
-  }, [status, router]);
+    const [submitting, setSubmiting] = useState(false);
+    const [error, setError] = useState("");
+    const [creds, setCreds] = useState<Credentials>({
+        email: "",
+        password: "",
+    });
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    setSubmiting(true);
-    signIn("credentials", {
-      ...creds,
-      redirect: false,
-      callbackUrl: "/chats",
-    })
-      .then((response) => {
-        if (!response?.ok && response?.error) setError(response.error);
-        router.replace(response?.url as string);
-      })
-      .catch((error) => {
-        setError("Internal Server Error");
-      })
-      .finally(() => setSubmiting(false));
-  };
+    useEffect(() => {
+        if (status === "authenticated") router.push("/chats");
+    }, [status, router]);
 
-  const handleSignInWithGithub = () => {
-    signIn("github", { redirect: false, callbackUrl: "/chats" })
-      .then((response) => {
-        if (!response?.ok && response?.error) setError(response.error);
-      })
-      .catch((error) => {
-        setError("Internal Server Error");
-      })
-      .finally(() => setSubmiting(false));
-  };
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        setSubmiting(true);
+        signIn("credentials", {
+            ...creds,
+            redirect: false,
+            callbackUrl: "/chats",
+        })
+            .then((response) => {
+                if (!response?.ok && response?.error) setError(response.error);
+                router.replace(response?.url as string);
+            })
+            .catch((error) => {
+                setError("Internal Server Error");
+            })
+            .finally(() => setSubmiting(false));
+    };
 
-  const handleSignInWithGoogle = () => {
-    signIn("google", { redirect: false, callbackUrl: "/chats" })
-      .then((response) => {
-        if (!response?.ok && response?.error) setError(response.error);
-      })
-      .catch((error) => {
-        setError("Internal Server Error");
-      })
-      .finally(() => setSubmiting(false));
-  };
+    const handleSignInWithGithub = () => {
+        signIn("github", { redirect: false, callbackUrl: "/chats" })
+            .then((response) => {
+                if (!response?.ok && response?.error) setError(response.error);
+            })
+            .catch(() => {
+                setError("Internal Server Error");
+            })
+            .finally(() => setSubmiting(false));
+    };
 
-  return (
-    <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <Image
-          width={80}
-          height={10}
-          className="mx-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign In to your account
-        </h2>
-      </div>
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form onSubmit={handleSubmit} className="space-y-6" method="POST">
-          <Input
-            onChange={(e) =>
-              setCreds((cred) => ({ ...cred, email: e.target.value }))
-            }
-            value={creds.email}
-            type="text"
-            label="Email Address"
-            name="email"
-          />
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+    const handleSignInWithGoogle = () => {
+        signIn("google", { redirect: false, callbackUrl: "/chats" })
+            .then((response) => {
+                if (!response?.ok && response?.error) setError(response.error);
+            })
+            .catch(() => {
+                setError("Internal Server Error");
+            })
+            .finally(() => setSubmiting(false));
+    };
+
+    return (
+        <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Sign In to your account
+                </h2>
+            </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form
+                    onSubmit={handleSubmit}
+                    className="space-y-6"
+                    method="POST"
                 >
-                  Forgot password?
-                </a>
-              </div>
-            </div>
-            <Input
-              onChange={(e) =>
-                setCreds((cred) => ({ ...cred, password: e.target.value }))
-              }
-              value={creds.password}
-              type="password"
-              name="password"
-            />
-          </div>
+                    <Input
+                        onChange={(e) =>
+                            setCreds((cred) => ({
+                                ...cred,
+                                email: e.target.value,
+                            }))
+                        }
+                        value={creds.email}
+                        type="text"
+                        label="Email Address"
+                        name="email"
+                    />
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label
+                                htmlFor="password"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                            >
+                                Password
+                            </label>
+                            <div className="text-sm">
+                                <a
+                                    href="#"
+                                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                                >
+                                    Forgot password?
+                                </a>
+                            </div>
+                        </div>
+                        <Input
+                            onChange={(e) =>
+                                setCreds((cred) => ({
+                                    ...cred,
+                                    password: e.target.value,
+                                }))
+                            }
+                            value={creds.password}
+                            type="password"
+                            name="password"
+                        />
+                    </div>
 
-          {error && (
-            <div className="rounded-lg bg-rose-500 p-2 px-4 text-white">
-              {error}
+                    {error && (
+                        <div className="rounded-lg bg-rose-500 p-2 px-4 text-white">
+                            {error}
+                        </div>
+                    )}
+                    <div>
+                        <button
+                            type="submit"
+                            disabled={submitting}
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:cursor-default disabled:opacity-50"
+                        >
+                            {submitting ? "Signing In" : "Sign In"}
+                        </button>
+                    </div>
+                </form>
+                <div className="my-10">
+                    <div className="mb-5 flex items-center gap-4">
+                        <hr className="flex-1" />
+                        <div className="text-sm">Or Continue With</div>
+                        <hr className="flex-1" />
+                    </div>
+                    <div className="flex gap-4">
+                        <button
+                            onClick={handleSignInWithGithub}
+                            className="flex w-full justify-center rounded-md bg-transparent p-3 text-sm font-semibold leading-6 text-gray-500 ring-1 ring-gray-500 hover:bg-indigo-600 hover:text-white hover:ring-indigo-600"
+                        >
+                            <FaGithub />
+                        </button>
+                        <button
+                            onClick={handleSignInWithGoogle}
+                            className="flex w-full justify-center rounded-md bg-transparent p-3 text-sm font-semibold leading-6 text-gray-500 ring-1 ring-gray-500 hover:bg-indigo-600 hover:text-white hover:ring-indigo-600"
+                        >
+                            <FaGoogle />
+                        </button>
+                    </div>
+                </div>
+                <div>
+                    <p className="text-center text-sm text-gray-500">
+                        Not a member?
+                        <a
+                            href="/sign-up"
+                            className="ml-2 font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                        >
+                            Create Account
+                        </a>
+                    </p>
+                </div>
             </div>
-          )}
-          <div>
-            <button
-              type="submit"
-              disabled={submitting}
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:cursor-default disabled:opacity-50"
-            >
-              {submitting ? "Signing In" : "Sign In"}
-            </button>
-          </div>
-        </form>
-        <div className="my-10">
-          <div className="mb-5 flex items-center gap-4">
-            <hr className="flex-1" />
-            <div className="text-sm">Or Continue With</div>
-            <hr className="flex-1" />
-          </div>
-          <div className="flex gap-4">
-            <button
-              onClick={handleSignInWithGithub}
-              className="flex w-full justify-center rounded-md bg-transparent p-3 text-sm font-semibold leading-6 text-gray-500 ring-1 ring-gray-500 hover:bg-indigo-600 hover:text-white hover:ring-indigo-600"
-            >
-              <FaGithub />
-            </button>
-            <button
-              onClick={handleSignInWithGoogle}
-              className="flex w-full justify-center rounded-md bg-transparent p-3 text-sm font-semibold leading-6 text-gray-500 ring-1 ring-gray-500 hover:bg-indigo-600 hover:text-white hover:ring-indigo-600"
-            >
-              <FaGoogle />
-            </button>
-          </div>
         </div>
-        <div>
-          <p className="text-center text-sm text-gray-500">
-            Not a member?
-            <a
-              href="/sign-up"
-              className="ml-2 font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              Create Account
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
